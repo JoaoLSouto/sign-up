bd5f1ca7a079fb81d54ee0c0d08cc0e6916af38a
JoaoLSouto <contato.joaolsouto@gmail.com>
2023-03-13 21:43:12 -0300

    Home Styles.js

--- sign-up-pages/src/pages/home/index.jsx
+++ sign-up-pages/src/pages/home/index.jsx
@@ -1,12 +1,30 @@
 import { Link } from "react-router-dom";
 
+import banner from "../../assets/banner.png"
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
-
+import {Container, TitleContent, Title, TitleHighlight  } from './styles';
 const Home = () => {
     return(<>
         <Header />
         <Link to="/login"> Fazer login </Link>
+        <Container>
+            <div>
+                <Title>
+                    <TitleHighlight>
+                        Implemente <br />
+                    </TitleHighlight>
+                    o seu futuro global
+                </Title>
+                <TitleContent>
+                    Lero Lero Bla Bla Lero Lero Bla Bla Lero Lero Bla BlaLero Lero Bla BlaLero Lero Bla Bla
+                </TitleContent>
+                <Button title="Começar" variant="secondary" onClick={() => null}/>
+            </div>
+            <div>
+                <img src={banner} alt="Imagem Principal" />   
+            </div>
+        </Container>
         </>
     );        
 };
