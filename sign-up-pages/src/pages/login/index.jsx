c74ed793ee574e0ff9a645a61ec178cff5bf1f2d
JoaoLSouto <contato.joaolsouto@gmail.com>
2023-03-28 11:07:26 -0300

    required Input fixs

--- sign-up-pages/src/pages/login/index.jsx
+++ sign-up-pages/src/pages/login/index.jsx
@@ -1,60 +1,57 @@
 import { useNavigate  } from "react-router-dom";
-import { useForm} from "react-hook-form";
+import { MdEmail, MdLock } from 'react-icons/md'
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
-
-import { api } from '../../services/api'
-
-import { MdEmail, MdLock } from 'react-icons/md'
+import { api } from '../../services/api';
 
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
+import { useForm } from "react-hook-form";
 
-import { Column, ErrorText ,Container, CriarText, EsqueciText, Row, SubtitleLogin, Title, TitleLogin, Wrapper} from './styles';
+
+import { Container, Title, Column, TitleLogin, SubtitleLogin, EsqueciText, CriarText, Row, Wrapper } from './styles';
 
 const schema = yup.object({
-    email: yup.string('Email não é valido').required(),
-    password: yup.number().min(3, 'No minimo 3 caracteres').required(),
+    email: yup.string().email('esse E-mail não é válido').required(),
+    password: yup.string().min(3,'No minimo 3 caracteres').required(),
   }).required();
 
 const Login = () => {
 
-    const navigate = useNavigate();
-    const { control, register, handleSubmit, watch, formState: { errors } } = useForm({
+    const navigate = useNavigate()
+
+    const { control, register, handleSubmit, watch, formState: { errors, isValid } } = useForm({
         resolver: yupResolver(schema),
-        mode: 'onChange'
+        mode: 'onChange',
     });
+    const onSubmit = data => console.log(data);
+    console.log('errors', errors);
 
-    console.log(errors)
-
-    const onSubmit = data => {
-        try {
-            const {} = api.get(`users?email=${formData.email}&senha=${formData.senha} `)
-        }catch{
-            alert('houve um erro!')
-        }
+    const handleClickSignIn = () => {
+        navigate('/feed')
     }
 
     return (<>
         <Header />
         <Container>
             <Column>
-                <Title> A plataforma para você aprender com experts, dominar as principais tecnologias
+                <Title>A plataforma para você aprender com experts, dominar as principais tecnologias
                  e entrar mais rápido nas empresas mais desejadas.</Title>
             </Column>
             <Column>
                 <Wrapper>
                 <TitleLogin>Faça seu cadastro</TitleLogin>
-                <SubtitleLogin> Faça seu Login</SubtitleLogin>
+                <SubtitleLogin>Faça seu login e make the change.</SubtitleLogin>
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <Input placeholder="E-mail" errormessage={errors.email.message}leftIcon={<MdEmail />} name="email"  control={control} />
-                    <Input type="password" placeholder="Senha "  errormessage={errors.password.message}  leftIcon={<MdLock />}  name="senha" control={control} />
-                    <Button title="Entrar" varian="secondary" type="submit"></Button>
+                    <Input  name="email" control={control} placeholder="E-mail" errorMessage={errors?.email?.message} leftIcon={<MdEmail />} name="email" />
+                    <Input  name="password" control={control} type="password" placeholder="Senha" errorMessage={errors?.password?.message} leftIcon={<MdLock />}  name="senha"/>
+                    
+                    <Button title="Entrar" variant="secondary" type="submit"/>
                 </form>
                 <Row>
                     <EsqueciText>Esqueci minha senha</EsqueciText>
-                    <CriarText> Criar conta</CriarText>
+                    <CriarText>Criar Conta</CriarText>
                 </Row>
                 </Wrapper>
             </Column>
